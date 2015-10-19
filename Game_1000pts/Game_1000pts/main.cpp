8acbf80a61562b8e861b653e3a7383c23211a8e9
Willis Pinaud <willispinaud@gmail.com>
2015-10-19 09:43:32 +0200

    project xcode

---
+++ Game_1000pts/Game_1000pts/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Game_1000pts
+//
+//  Created by Willis Pinaud on 19/10/2015.
+//  Copyright © 2015 Centrale. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
