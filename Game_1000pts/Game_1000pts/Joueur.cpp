878dcd996c37c3f9af9d858402b87f3acaa128da
Evileyrik <eric.lalevee@gmail.com>
2015-10-21 09:17:26 +0200

    update estjouable

--- Game_1000pts/Game_1000pts/Joueur.cpp
+++ Game_1000pts/Game_1000pts/Joueur.cpp
@@ -20,7 +20,36 @@ Carte* Joueur::getFirstOnDeck(){
     return tas[tas.end()]; //Vérfier que c'est bien le premier élement
 }
 
-bool Joueur::estJouable(Carte* carte){
-    //Si la premiere carte est pas compatible elle n'est pas jouable
-    return true;
+bool Joueur::estJouable(Carte* carte, Carte* tasAdv){
+    //la carte donnée est une carte point
+    // et la première carte de mon tas est une point ou une parade
+    if (carte == "point"){
+        if (getFirstOnDeck() == "point" || getFirstOnDeck() == "P"){
+            return true;
+        }
+        else{ // et la première carte de mon tas est une attaque
+            return false;
+        }
+    }
+
+    //La carte donnée est une carte attaque
+    // et la première carte du tas de l'adversaire est une point ou une parade
+    if (carte == "A"){
+        if (tasAdv == "point" || tasAdv == "P"){
+            return true;
+        }
+        else{ // et la première carte du tas de l'adversaire est une attaque
+            return false;
+        }
+    }
+    //La carte donnée est une carte parade
+    // et la première carte de mon tas est une point ou une parade
+    if (carte == "P"){
+        if (getFirstOnDeck() == "point" || getFirstOnDeck() == "P"){
+            return false;
+        }
+        else{ // et la première carte de mon tas est une attaque
+            return true;
+        }
+    }
 }
